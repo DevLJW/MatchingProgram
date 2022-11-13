9b99960861dc70685106bfa7fe0fa4dfb4cec23a
DevLJW <jaewookv123@gmail.com>
2022-11-14 02:01:33 +0900

    FireBase RealtimeDatabase uid,name Column add

--- app/src/main/java/com/example/matching/LikeActivity.kt
+++ app/src/main/java/com/example/matching/LikeActivity.kt
@@ -1,10 +1,15 @@
 package com.example.matching
 
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
@@ -19,9 +24,25 @@ class LikeActivity : AppCompatActivity() {
 
         val userDB = Firebase.database.reference.child("Users")
 
-        val currentUserDB = userDB.child(auth.currentUser!!.uid.orEmpty())
+        val currentUserDB = userDB.child(getCurrentUserID())
+        currentUserDB.addListenerForSingleValueEvent(object :ValueEventListener{
 
-    }
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+        //처음에 리스너를 달았을때는 리스너가 없었기 때문에 OnDatachange로 넘어오고 또, 데이터 변경시 한번 내려온다.
+                if(snapshot.child("name").value == null){ //네임 컬럼의 값이 없으면
+                    showNameInputPop()
+                    return
+                }
+
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+        }
 
     private fun getCurrentUserID(): String {
         if (auth.currentUser == null) {
@@ -30,6 +51,37 @@ class LikeActivity : AppCompatActivity() {
         }
 
             return auth.currentUser!!.uid
+
+    }
+
+    private fun showNameInputPop() {
+        val editText = EditText(this)
+
+        AlertDialog.Builder(this).setTitle("이름을 입력해주세요").setView(editText)
+            .setPositiveButton("저장"){_,_->
+                if(editText.text.isEmpty()){
+                    showNameInputPop()
+
+
+                }else{
+                    saveUserName(editText.text.toString())
+                }
+
+
+            }.setCancelable(false)
+            .show()
+
+    }
+
+    private fun saveUserName(name: String){
+
+        val userId = getCurrentUserID()
+        val currentUserDB =Firebase.database.reference.child("Users").child(userId) //최상위 레퍼런스 중에서 Users 선택 없으면 하나 생성
+        val user = mutableMapOf<String,Any>()
+        user["userId"] = userId
+        user["name"] = name
+        currentUserDB.updateChildren(user)
+
 
     }
 }
